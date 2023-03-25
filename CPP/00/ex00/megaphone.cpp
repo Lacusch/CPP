26d63edf96b90b3eae580abaccc3efb0e547919b
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2023-03-25 09:19:27 +0100

    Follow Google c++ formating style

--- CPP/00/ex00/megaphone.cpp
+++ CPP/00/ex00/megaphone.cpp
@@ -1,24 +1,21 @@
-#include <iostream>
 #include <cctype>
+#include <iostream>
 
-int main (int ac, char **av)
-{
-	int i;
-	char ch;
+int main(int ac, char **av) {
+  int i;
+  char ch;
 
-	i = 0;
-	if (ac == 1)
-	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return 0;
-	}
-	else
-		for (int i = 1; i < ac; i++) {
-			for (int i2 = 0; i2 < (int)strlen(av[i]); i2++) {
-				ch = std::toupper((unsigned char)av[i][i2]);
-				std::cout << ch;
-			}
-		}
-	std:: cout << std::endl;
-	return 0;
+  i = 0;
+  if (ac == 1) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    return 0;
+  } else
+    for (int i = 1; i < ac; i++) {
+      for (int i2 = 0; i2 < (int)strlen(av[i]); i2++) {
+        ch = std::toupper((unsigned char)av[i][i2]);
+        std::cout << ch;
+      }
+    }
+  std::cout << std::endl;
+  return 0;
 }
--- CPP/00/ex01/Contact.hpp
+++ CPP/00/ex01/Contact.hpp
@@ -1,21 +1,22 @@
 #ifndef CONTACT_HPP
 #define CONTACT_HPP
+#include <iomanip>
 #include <iostream>
 #include <string>
-#include <iomanip>
 
 class Contact {
 public:
-	Contact ();
-	~Contact ();
-	void	get_info();
-	void	print_elements();
-	std::string *get_element(int i);
+  Contact();
+  ~Contact();
+  void get_info();
+  void print_elements();
+  std::string *get_element(int i);
+
 private:
-	std::string	_F_Name;
-	std::string _L_Name;
-	std::string _Nickname;
-	std::string	_Phone;
-	std::string _dark_secret;
+  std::string _F_Name;
+  std::string _L_Name;
+  std::string _Nickname;
+  std::string _Phone;
+  std::string _dark_secret;
 };
 #endif
