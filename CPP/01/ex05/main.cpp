ae319731c51daf766fe19f783530f4ea37634953
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2023-03-27 15:47:18 +0200

    Module 01/ex05
    Harl 2.0

---
+++ CPP/01/ex05/main.cpp
@@ -0,0 +1,10 @@
+#include "Harl.hpp"
+
+int main() {
+  Harl NPC;
+  NPC.complain("DEBUG");
+  NPC.complain("INFO");
+  NPC.complain("WARNING");
+  NPC.complain("ERROR");
+  NPC.complain("INVALID");
+}
