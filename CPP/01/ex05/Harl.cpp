931770b44f143be7b2dcc8e466b5174902c505b9
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2023-03-27 15:47:18 +0200

    Module 01/ex05
    Harl 2.0

---
+++ CPP/01/ex05/Harl.cpp
@@ -0,0 +1,52 @@
+#include "Harl.hpp"
+#include <sys/_types/_size_t.h>
+
+Harl::Harl() {
+  funcTable.f[0] = &Harl::_debug;
+  funcTable.n[0] = "DEBUG";
+  funcTable.f[1] = &Harl::_info;
+  funcTable.n[1] = "INFO";
+  funcTable.f[2] = &Harl::_warning;
+  funcTable.n[2] = "WARNING";
+  funcTable.f[3] = &Harl::_error;
+  funcTable.n[3] = "ERROR";
+}
+
+Harl::~Harl(){};
+
+void Harl::_debug(void) const {
+  std::cout << "I love having extra bacon for my "
+               "7XL-double-cheese-triple-pickle-special-ketchup "
+               "burger.I really do !"
+            << std::endl;
+}
+
+void Harl::_info(void) const {
+  std::cout
+      << "I cannot believe adding extra bacon costs more money. You didn’t put "
+         "enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+      << std::endl;
+}
+
+void Harl::_warning(void) const {
+  std::cout
+      << "I think I deserve to have some extra bacon for free. I’ve been "
+         "coming foryears whereas you started working here since last month."
+      << std::endl;
+}
+
+void Harl::_error(void) const {
+  std::cerr << "This is unacceptable! I want to speak to the manager now."
+            << std::endl;
+}
+
+void Harl::complain(std::string level) {
+  for (size_t i = 0; i < funcTable.n->size(); i++) {
+    if (!level.compare(funcTable.n[i])) {
+      (this->*funcTable.f[i])();
+      return;
+    }
+  }
+  std::cout << "[ Probably complaining about insignificant problems ]"
+            << std::endl;
+}
