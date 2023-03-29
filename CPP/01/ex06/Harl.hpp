60e080054e2017a778b4a536767be9850a85d895
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2023-03-29 14:23:52 +0200

    Module 01/ex06
    Harl filter

---
+++ CPP/01/ex06/Harl.hpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <system_error>
+class Harl {
+public:
+  Harl();
+  ~Harl();
+  void complain(std::string level);
+
+private:
+  void _debug(void) const;
+  void _info(void) const;
+  void _warning(void) const;
+  void _error(void) const;
+  typedef struct Funtions {
+    void (Harl::*f[4])(void) const;
+    std::string n[4];
+  } Funtions;
+  Funtions _funcTable;
+};
