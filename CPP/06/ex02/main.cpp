d9dbecd8711bd1032ffeb84c26f5f7c467a5c1a3
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2023-08-16 15:35:05 +0200

    update

--- CPP/06/ex02/main.cpp
+++ CPP/06/ex02/main.cpp
@@ -1,9 +1,11 @@
 #include <cstddef>
+
 #include "A.hpp"
 #include "B.hpp"
 #include "Base.hpp"
 #include "C.hpp"
 #include "identify.hpp"
+#include "iostream"
 
 int main() {
   Identity identity;
@@ -12,10 +14,10 @@ int main() {
   Base &ref = *to_ref;
   Base badCastTest;
   Base *iAmNull = NULL;
-
+  std::cout << "Valid tests:" << std::endl;
   identity.identify(ptr);
   identity.identify(ref);
-
+  std::cout << "Invalid tests:" << std::endl;
   identity.identify(iAmNull);
   identity.identify(badCastTest);
   delete ptr;
